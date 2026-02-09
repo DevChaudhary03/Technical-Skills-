63b097a23563b32d8ce75fb91264a6f756bd690d
devchaudhary2007 <devchaudharyak47@gamil.com>
2026-02-09 11:10:02 +0530

    target sum

---
+++ checksum.java
@@ -0,0 +1,18 @@
+public class checksum {
+    public static void main(String[] args) {
+        int []arr={3,-2,1,4,3,6,8};
+        int k=10;
+
+        for(int i =0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i !=j && arr[i]+ arr[j]==k){
+                    System.out.println(arr[i]+" "+ arr[j]);
+                    break;
+                }
+
+                    
+            }
+        }
+        }
+    }
+
